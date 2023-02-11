0fa420b407576e4f36e14c0f95718a75751165fc
Hugo Paré <hugopare12@gmail.com>
2023-02-11 13:23:24 -0500

    changed http request

--- uottahack-5/src/passwords/Generate.js
+++ uottahack-5/src/passwords/Generate.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import './Generate.css';
 
 class Generate extends Component{
 
@@ -7,14 +8,27 @@ class Generate extends Component{
         numbers: false,
         capitals: false,
         symbols: false,
-        value: 14
+        value: 14,
+        generatedPassword: false
     }
 
     async generatePassword(e){
         e.preventDefault();
         console.log("generate new password");
-        const response = await axios.get("http://localhost:8080/",{ params: {length: this.state.value}});
-        console.log(response.data);
+
+        fetch(`http://localhost:8080/api/password/${this.state.value}/${this.state.value}`, {
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Origin': 'http://localhost:3000'
+            }
+        })
+        .then(response => console.log(response))
+
+        // const response = await axios.get("http://localhost:8080/",{ params: {length: this.state.value}});
+        // console.log(response.data);
+        this.setState({generatePassword: true})
         // console.log("numbers : " + this.state.numbers);
         // console.log("capitals : " + this.state.capitals);
         // console.log("symbols : " + this.state.symbols);
@@ -41,24 +55,45 @@ class Generate extends Component{
         // console.log(event.target.checked);
     }
 
+    copyMyText(){
+        var textToCopy = document.getElementById("copyMe");
+        textToCopy.select();
+        document.execCommand("copy");
+    }
 
     render(){
+
         return(
             <>
-                <h1>Password Generator</h1>
-                <p>Choose a secure password using our tool !</p>
+                <div className="body">
+                    <h1>Password Generator</h1>
+                    <p>Choose a secure password using our tool !</p>
 
-                <input type="checkbox" id="numbers" onClick={this.changeNumbersBox}></input>
-                <label htmlFor="numbers">Include numbers</label><br></br>
-                <input type="checkbox" id="capitals" onClick={this.changeCapitalsBox}></input>
-                <label htmlFor="capitals">Include capitals letters</label><br></br>
-                <input type="checkbox" id="symbols" onClick={this.changeSymbolsBox}></input>
-                <label htmlFor="symbols">Include symbols</label><br></br>
+                    <div className="checkboxes">
+                        <input type="checkbox" id="numbers" onClick={this.changeNumbersBox}></input>
+                        <label htmlFor="numbers">Include numbers</label><br></br>
+                        <input type="checkbox" id="capitals" onClick={this.changeCapitalsBox}></input>
+                        <label htmlFor="capitals">Include capitals letters</label><br></br>
+                        <input type="checkbox" id="symbols" onClick={this.changeSymbolsBox}></input>
+                        <label htmlFor="symbols">Include symbols</label><br></br>
+                    </div>
+                    
 
-                <div className="rangeslider">
-                    <input type="range" min="8" max="20" value={this.value} step={1} onChange={this.changeValue} id="sliderRange"/>
+                    <div className="slider-container">
+                        <input type="range" className="slide" id="slider" min="8" max="20" value={this.value} step={1} onChange={this.changeValue}/>
+                    </div>
+
+                    <div className="button">
+                        <button onClick={(e) => this.generatePassword(e)}>Generate</button>
+                    </div>
+
+                    {this.state.generatedPassword ? 
+                        <div>
+                            <input type="text" value="123Password!" readOnly id="copyMe"></input>
+                            <button onClick={this.copyMyText}>Copy To Clipboard</button>
+                        </div> 
+                    : <div></div>}
                 </div>
-                <button onClick={(e) => this.generatePassword(e)}>Generate</button>
             </>
         )
     }
